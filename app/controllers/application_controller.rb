acfb374428fe51c9b05569cfa33180b169682213
TarrinRos <tarindis@gmail.com>
2019-03-09 12:21:33 +0200

    Remove app/assets and move javascript folder to root renamed as fronend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  # The way wich used by rails to find ERB partials:
+  prepend_view_path Rails.root.join("frontend")
 end
